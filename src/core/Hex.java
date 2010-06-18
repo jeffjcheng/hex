a75f21c2c867a392cf3ce8c8c8180d05f7efc10b
simon <simon@UAlpha.(none)>
2010-06-18 14:02:46 -0700

    Added Template source files to branch

---
+++ src/core/Hex.java
@@ -0,0 +1,18 @@
+package core;
+
+import java.awt.Dimension;
+
+import graphics.Gui;
+
+
+
+public class Hex {
+
+	public static void main(String[] args) {
+		Gui screen = new Gui("Hex Game Main Window");
+		screen.setSize(new Dimension(500,500));
+		screen.setVisible(true);
+		
+
+	}
+}
