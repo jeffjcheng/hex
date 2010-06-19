a2d7054b098c688ce2196b8cfcbe67f40b1e5a84
simon <simon@UAlpha>
2010-06-18 18:15:03 -0700

    Added Template for Hex Connector class, so far it does nothing

---
+++ src/net/HexConnector.java
@@ -0,0 +1,17 @@
+package net;
+/*
+ * This is the connector object, it is instantiated once the game is run.
+ * It is meant to serve as a tunnel between clients.
+ * 
+ * 
+ */
+
+import java.net.Socket;
+import java.net.*;
+
+public class HexConnector {
+	public HexConnector(){
+		
+	}
+
+}
